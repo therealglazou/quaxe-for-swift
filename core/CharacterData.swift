a486b7fa0963b066f41764318aca05da9bec4f17
Daniel Glazman <daniel@glazman.org>
2016-02-25 13:03:44 +0100

    final CharacterData

--- core/CharacterData.swift
+++ core/CharacterData.swift
@@ -20,11 +20,38 @@ public class CharacterData: Node, pCharacterData {
   public var length: ulong {
     return ulong(data.characters.count)
   }
-  public func substringData(offset: ulong, _ count: ulong) -> DOMString { return ""}
-  public func appendData(data: DOMString) -> Void {}
-  public func insertData(offset: ulong, _ data: DOMString) -> Void {}
-  public func deleteData(offset: ulong, _ count: ulong) -> Void {}
-  public func replaceData(offset: ulong, _ count: ulong, _ data: DOMString) -> Void {}
+  public func substringData(offset: ulong, _ count: ulong) -> DOMString {
+    let index1 = self.data.startIndex.advancedBy(Int(offset))
+    let index2 = self.data.startIndex.advancedBy(Int(offset + count))
+    return self.data.substringWithRange(index1...index2)
+  }
+
+  public func appendData(str: DOMString) -> Void {
+    self.data += str
+  }
+
+  public func insertData(offset: ulong, _ str: DOMString) -> Void {
+    let index1 = self.data.startIndex.advancedBy(Int(offset))
+    let preData = self.data.substringToIndex(index1)
+    let postData = self.data.substringFromIndex(index1)
+    self.data = preData + str + postData
+  }
+
+  public func deleteData(offset: ulong, _ count: ulong) -> Void {
+    let index1 = self.data.startIndex.advancedBy(Int(offset))
+    let index2 = self.data.startIndex.advancedBy(Int(offset + count))
+    let preData = self.data.substringToIndex(index1)
+    let postData = self.data.substringFromIndex(index2)
+    self.data = preData + postData
+  }
+
+  public func replaceData(offset: ulong, _ count: ulong, _ str: DOMString) -> Void {
+    let index1 = self.data.startIndex.advancedBy(Int(offset))
+    let index2 = self.data.startIndex.advancedBy(Int(offset + count))
+    let preData = self.data.substringToIndex(index1)
+    let postData = self.data.substringFromIndex(index2)
+    self.data = preData + str + postData
+  }
 
   override init() {
     super.init()
