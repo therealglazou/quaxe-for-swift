c331e4d328afb9c474eb21c85b257ef8f7ac9990
Daniel Glazman <daniel@glazman.org>
2016-02-25 11:59:21 +0100

    HTMLCollection

--- core/HTMLCollection.swift
+++ core/HTMLCollection.swift
@@ -13,9 +13,39 @@
 
 
 public class HTMLCollection: pHTMLCollection {
-  public var length: ulong = 0
-  public func item(index: ulong) -> pElement? {return nil}
-  public func namedItem(name: DOMString) -> pElement? {return nil}
+
+  internal func append(element: pElement) -> Void {
+    mArray.append(element)
+  }
+
+  internal func clobber() -> Void {
+    mArray = []
+  }
+
+  internal var mArray: Array<pElement> = []
+  
+  public var length: ulong {
+    return ulong(self.mArray.count)
+  }
+
+  public func item(index: ulong) -> pElement? {
+    if index >= self.length {
+      return nil
+    }
+    return mArray[Int(index)]
+  }
+
+  public func namedItem(name: DOMString) -> pElement? {
+    let index = mArray.indexOf({ return $0.getAttribute("id") == name})
+    if nil != index {
+      return mArray[index!]
+    }
+    return nil
+  }
 
   init() {}
+
+  init(_ elements: Array<pElement>) {
+    mArray = elements
+  }
 }
