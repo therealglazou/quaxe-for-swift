dd311ffa44fca77540fbf04399c06487a6561319
Daniel Glazman <daniel@glazman.org>
2016-03-04 11:41:01 +0100

    DOMImplementation.createDocumentType

--- quaxe/core/DOMImplementation.swift
+++ quaxe/core/DOMImplementation.swift
@@ -10,8 +10,25 @@
  * 
  */
 
+/**
+ * https://dom.spec.whatwg.org/#interface-domimplementation
+ */
 public class DOMImplementation: pDOMImplementation {
-  public func createDocumentType(qualifiedName: DOMString, _ publicId: DOMString, _ systemId: DOMString) -> pDocumentType {return DocumentType()}
+
+  /**
+   * https://dom.spec.whatwg.org/#dom-domimplementation-createdocumenttype
+   */
+  public func createDocumentType(qualifiedName: DOMString, _ publicId: DOMString, _ systemId: DOMString) throws -> pDocumentType {
+    //Step 1
+    try Namespaces.validateQualifiedName(qualifiedName)
+
+    // Step 2
+    let dt = DocumentType()
+    dt.mName = qualifiedName
+    dt.mPublicId = publicId
+    dt.mSystemId = systemId
+    return dt
+  }
   public func createDocument(namespace: DOMString?, _ qualifiedName: DOMString?, _ doctype: pDocumentType) -> pXMLDocument {return Document()}
   public func createHTMLDocument(title: DOMString) -> pDocument {return Document()}
 
